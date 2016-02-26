461c0da4960843ce2696f8adce51d8227088c20e
Noah Klein <noahklein94@gmail.com>
2016-02-26 00:50:34 -0500

    Tests added

---
+++ src/test/scala/Test.scala
@@ -0,0 +1,34 @@
+package com.tictactoe
+
+import org.scalatest._
+import Board._
+
+class BoardTest extends FlatSpec {
+  "testBoard.won" should "be Some(X)" in {
+    val board = List(
+        List(Some(X), Some(O), Some(O)),
+        List(Some(X), None, Some(X)),
+        List(Some(X), None, Some(O)))
+    val testBoard = new Board(board)
+    assert(testBoard.won == Some(X))
+  }
+
+  "testBoard.won" should "be Some(O)" in {
+    val board = List(
+        List(Some(X), Some(O), Some(X)),
+        List(Some(X), Some(O), None),
+        List(Some(O), Some(O), Some(X)))
+    val testBoard = new Board(board)
+    assert(testBoard.won == Some(O))
+  }
+
+  "playersPositions" should "be Set(1, 4, 5, 7, 8)" in {
+    val board = List(
+      List(None, Some(O), Some(X)),
+      List(Some(X), Some(O), Some(O)),
+      List(Some(X), Some(O), Some(O)))
+    val testBoard = new Board(board)
+    assert(testBoard.playerPositions(O) == Set(1, 4, 5, 7, 8))
+    assert(testBoard.playerPositions(X) == Set(2, 3, 6))
+  }
+}
